c653dedfe6499ae772dcedb383a0eef2364fd36a
Chris Schneider <chris@christopher-schneider.com>
2013-07-15 21:58:51 -0600

    Add / Remove items from the stack

--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -1,13 +1,27 @@
-angular.module("app").controller('HomeController', function($scope, $location, AuthenticationService) {
-  $scope.title = "Home";
-  $scope.message = "Mouse Over these images to see a directive at work";
+angular.module("app").controller('HomeController', function($scope) {
+  var noot_from_name = function(name) {
+    return { 'name': name }
+  }
 
-  var onLogoutSuccess = function(response) {
-    alert(response.message);
-    $location.path('/login');
-  };
+  $scope.title = "NootStacks";
 
-  $scope.logout = function() {
-    AuthenticationService.logout().success(onLogoutSuccess);
-  };
+  $scope.noots = [
+    noot_from_name("Piracetam"),
+    noot_from_name("Oxiracetam"),
+    noot_from_name("Aniracetam"),
+    noot_from_name("Nefiracetam"),
+    noot_from_name("Pramiracetam"),
+    noot_from_name("Phenylpiracetam"),
+    noot_from_name("Modafinil")
+  ]
+
+  $scope.stack = {};
+
+  $scope.add = function(noot) {
+    $scope.stack[noot] = noot_from_name(noot);
+  }
+
+  $scope.remove = function(noot) {
+    delete $scope.stack[noot];
+  }
 });
